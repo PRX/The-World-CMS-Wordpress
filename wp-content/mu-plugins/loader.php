5796d17719b495f6d1a4f065c4620ef17ddeb5f9
Rick Peterman <rick.peterman@prx.org>
2025-07-24 15:12:28 -0500

    chore(mu plugin loader): cleanup debugging code

--- wp-content/mu-plugins/loader.php
+++ wp-content/mu-plugins/loader.php
@@ -12,67 +12,75 @@ if ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) {
 }
 
 // Add mu-plugins here.
-$mu_plugins = [
-	'the-world-site-config/the-world-site-config.php'
-];
+$mu_plugins = array(
+	'the-world-site-config/the-world-site-config.php',
+);
 
 foreach ( $mu_plugins as $file ) {
 	require_once WPMU_PLUGIN_DIR . '/' . $file;
 }
 unset( $file );
 
-add_action( 'pre_current_active_plugins', function () use ( $mu_plugins ) {
-	global $plugins, $wp_list_table;
+add_action(
+	'pre_current_active_plugins',
+	function () use ( $mu_plugins ) {
+		global $plugins, $wp_list_table;
 
-	error_log( print_r( $mu_plugins, true ) );
+		// Add our own mu-plugins to the page.
+		foreach ( $mu_plugins as $plugin_file ) {
+			// Do not apply markup/translate as it'll be cached.
+			$plugin_data = get_plugin_data( WPMU_PLUGIN_DIR . "/$plugin_file", false, false );
 
-	// Add our own mu-plugins to the page.
-	foreach ( $mu_plugins as $plugin_file ) {
-		// Do not apply markup/translate as it'll be cached.
-		$plugin_data = get_plugin_data( WPMU_PLUGIN_DIR . "/$plugin_file", false, false );
+			if ( empty( $plugin_data['Name'] ) ) {
+				$plugin_data['Name'] = $plugin_file;
+			}
 
-		if ( empty( $plugin_data['Name'] ) ) {
-			$plugin_data['Name'] = $plugin_file;
+			$plugins['mustuse'][ $plugin_file ] = $plugin_data;
 		}
 
-		$plugins['mustuse'][ $plugin_file ] = $plugin_data;
+		// Recount totals.
+		$GLOBALS['totals']['mustuse'] = count( $plugins['mustuse'] );
+
+		// Only apply the rest if we're actually looking at the page.
+		if ( $GLOBALS['status'] !== 'mustuse' ) {
+			return;
+		}
+
+		// Reset the list table's data.
+		$wp_list_table->items = $plugins['mustuse'];
+		foreach ( $wp_list_table->items as $plugin_file => $plugin_data ) {
+			$wp_list_table->items[ $plugin_file ] = _get_plugin_data_markup_translate( $plugin_file, $plugin_data, false, true );
+		}
+
+		$total_this_page = $GLOBALS['totals']['mustuse'];
+
+		if ( $GLOBALS['orderby'] ) {
+			uasort( $wp_list_table->items, array( $wp_list_table, '_order_callback' ) );
+		}
+
+		// Force showing all plugins.
+		// See https://core.trac.wordpress.org/ticket/27110.
+		$plugins_per_page = $total_this_page;
+
+		$wp_list_table->set_pagination_args(
+			array(
+				'total_items' => $total_this_page,
+				'per_page'    => $plugins_per_page,
+			)
+		);
 	}
+);
 
-	// Recount totals.
-	$GLOBALS['totals']['mustuse'] = count( $plugins['mustuse'] );
+add_filter(
+	'network_admin_plugin_action_links',
+	function ( $actions, $plugin_file, $plugin_data, $context ) use ( $mu_plugins ) {
+		if ( $context !== 'mustuse' || ! in_array( $plugin_file, $mu_plugins, true ) ) {
+			return $actions;
+		}
 
-	// Only apply the rest if we're actually looking at the page.
-	if ( $GLOBALS['status'] !== 'mustuse' ) {
-		return;
-	}
-
-	// Reset the list table's data.
-	$wp_list_table->items = $plugins['mustuse'];
-	foreach ( $wp_list_table->items as $plugin_file => $plugin_data ) {
-		$wp_list_table->items[ $plugin_file ] = _get_plugin_data_markup_translate( $plugin_file, $plugin_data, false, true );
-	}
-
-	$total_this_page = $GLOBALS['totals']['mustuse'];
-
-	if ( $GLOBALS['orderby'] ) {
-		uasort( $wp_list_table->items, [ $wp_list_table, '_order_callback' ] );
-	}
-
-	// Force showing all plugins.
-	// See https://core.trac.wordpress.org/ticket/27110.
-	$plugins_per_page = $total_this_page;
-
-	$wp_list_table->set_pagination_args( [
-		'total_items' => $total_this_page,
-		'per_page'    => $plugins_per_page,
-	] );
-});
-
-add_filter( 'network_admin_plugin_action_links', function ( $actions, $plugin_file, $plugin_data, $context ) use ( $mu_plugins ) {
-	if ( $context !== 'mustuse' || ! in_array( $plugin_file, $mu_plugins, true ) ) {
+		$actions[] = sprintf( '<span style="color:#333">File: <code>%s</code></span>', $plugin_file );
 		return $actions;
-	}
-
-	$actions[] = sprintf( '<span style="color:#333">File: <code>%s</code></span>', $plugin_file );
-	return $actions;
-}, 10, 4 );
+	},
+	10,
+	4
+);
